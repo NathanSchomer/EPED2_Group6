e5122cea690e5bdf67c292cc3a1e8a70cec65276
NathanSchomer <nathan@nathanschomer.com>
2015-07-03 12:19:14 -0400

    Added Q1 to Biweekly1.m and removed extra file.

--- Biweekly1.m
+++ Biweekly1.m
@@ -1,10 +1,48 @@
 close all
 clear
 clc
+
+%% 1. Basics
+
+v1 = rand(1,1000); %vector of 1000 random numbers
+
+greaterThanFive = 0;    %count numbers greater than 0.5
+between3and7 = 0;       %count numbers between 0.3 and 0.7
+
+for n = 1:length(v1) %loop through vector
+    
+    %increment counter if number is greater than 5
+    if v1(n) > 0.5
+        greaterThanFive = greaterThanFive + 1;
+    end
+    
+    %increment counter if number is between 0.3 and 0.7
+    if v1(n) > 0.3 && v1(n) < 0.7
+        between3and7 = between3and7 + 1;
+    end
+end
+
+%create histogram of random numbers with labels
+histogram(v1,10);
+grid on
+title('1000 Random Numbers')
+xlabel('bins')
+ylabel('frequency')
+
+%calculate & display mean and standard deviation
+fprintf('\nMean of numbers: \t\t%f', mean(v1));
+fprintf('\nStandard deviation of numbers: \t%f', std(v1));
+
+%display counters
+fprintf('\nNumbers greater than 0.5: \t%d', greaterThanFive);
+fprintf('\nNumbers between 0.3 and 0.7: \t%d', between3and7);
+
 %% Random Walks
+clear
+clc
 
 % Question 2
-% Initialize position of x
+% Initialize position of x, set up array for number of steps
 xposition = 0;
 xstep = zeros(1, 10);
 xwalk = randi(2, 1, 10);
@@ -15,7 +53,7 @@ for i = 1:length(xwalk)
     else
         xposition = xposition + 1;
     end
-    xstep(i) = xposition;   % Place positions into an array
+    xstep(i) = xposition;   % Place positions into the array
 end
 
 distance = max(abs(xstep));
@@ -27,9 +65,11 @@ xlabel 'Time'
 ylabel 'Position'
 title 'Question 2'
 axis ([0 10 -10 10])
+grid on
 
-%% Question 3
+% Question 3
 
+% Initialize positions and set up the arrays
 newxpos = 0;
 newypos = 0;
 newxstep = zeros(1, 25);
@@ -46,11 +86,14 @@ for i = 1:length(bothwalk)
     else
         newypos = newypos - 1;
     end
+    % Place the steps into the arrays
     newxstep(i) = newxpos;
     newystep(i) = newypos;
 end
 
+% Place the arrays into a 2 x 26 matrix to be plotted
 bothstep = [0 newxstep; 0 newystep];
+
 figure
 plot(bothstep(1,:), bothstep(2,:), 'o:')
 xlabel 'Time'
@@ -58,17 +101,19 @@ ylabel 'Position'
 title 'Question 3'
 axis equal
 hold on
+grid on
 plot(bothstep(1), bothstep(2), 'o', 'MarkerSize', 20, 'Color', 'g')
 plot(bothstep(end-1), bothstep(end), 'o', 'MarkerSize', 20, 'Color', 'r')
 
 %% Question 4a
 
-iterations = zeros(1, 5000);
+% Set up the array
+twoperson_iterations = zeros(1, 5000);
 
 for i = 1:5000
     count = 0;
     % initialize position for each person, set x and y coordinates
-    inip1pos = randi(100, 1, 2)'; 
+    inip1pos = randi(100, 1, 2)';
     inip2pos = randi(100, 1, 2)';
     p1x = inip1pos(1);
     p1y = inip1pos(2);
@@ -77,10 +122,13 @@ for i = 1:5000
     % finds the distance between the two characters
     distx = abs(p1x - p2x);
     disty = abs(p1y - p2y);
-    unitdist = sqrt(distx^2 + disty^2);
-    while unitdist > 5
+    unitdist2 = sqrt(distx^2 + disty^2);
+    % As long as the two characters are more than five units away, continue
+    % running the loop
+    while unitdist2 > 5
         p1walk = randi(4, 1, 1);
         p2walk = randi(4, 1, 1);
+        % Base direction of walking on value for random integer
         if p1walk == 1
             p1x = p1x + 1;
         elseif p1walk == 2
@@ -90,6 +138,7 @@ for i = 1:5000
         else
             p1y = p1y - 1;
         end
+        % If the character goes out of bounds, make them turn around
         if p1x >= 100
             p1x = p1x - 1;
         end
@@ -123,31 +172,73 @@ for i = 1:5000
         if p2y <= 0
             p2y = p2y + 1;
         end
+        % Re-calculate the distance at the end of each iteration
         distx = abs(p1x - p2x);
         disty = abs(p1y - p2y);
-        unitdist = sqrt(distx^2 + disty^2);
+        unitdist2 = sqrt(distx^2 + disty^2);
         count = count + 1;
     end
-    iterations(i) = count;
+    twoperson_iterations(i) = count;
 end
+
+avg_twoiterations = mean(twoperson_iterations);
+fprintf('The average number of time steps it takes for the characters to find each other is %.2f steps. \n', avg_twoiterations)
+figure
+hist(twoperson_iterations)
+title('Number of Time Steps - Two People Moving')
+xlabel('Bins')
+ylabel('Frequency')
 
 %% Question 4b
 
-count = 0;
-personA = randi(100, 1, 2)';
-personB = randi(100, 1, 2)';
-pAx = personA(1);
-pAy = personA(2);
-pBx = personB(1);
-pBy = personB(2);
-distx = abs(pAx - pBx);
-disty = abs(pAy - pBy);
-unitdist = sqrt(distx^2 + disty^2);
+oneperson_iterations = zeros(1, 5000);
 
+for i = 1:length(oneperson_iterations)
+    count = 0;
+    personA = randi(100, 1, 2)';
+    personB = randi(100, 1, 2)';
+    pAx = personA(1);
+    pAy = personA(2);
+    pBx = personB(1);
+    pBy = personB(2);
+    distx = abs(pAx - pBx);
+    disty = abs(pAy - pBy);
+    unitdist1 = sqrt(distx^2 + disty^2);
+    while unitdist1 > 5
+        pAwalk = randi(4, 1, 1);
+        if pAwalk == 1
+            pAx = pAx + 1;
+        elseif pAwalk == 2
+            pAx = pAx - 1;
+        elseif pAwalk ==3
+            pAy = pAy + 1;
+        else pAy = pAy - 1;
+        end
+        if pAx >= 100
+            pAx = pAx - 1;
+        end
+        if pAy >= 100
+            pAy = pAy -1;
+        end
+        if pAx <= 0
+            pAx = pAx + 1;
+        end
+        if pAy <= 0
+            pAy = pAy + 1;
+        end
+        % Re-calculate the distance at the end of each iteration
+        distx = abs(pAx - pBx);
+        disty = abs(pAy - pBy);
+        unitdist1 = sqrt(distx^2 + disty^2);
+        count = count + 1;
+    end
+    oneperson_iterations(i) = count;
+end
 
-
-
-
-
-
-
+avg_oneiterations = mean(oneperson_iterations);
+fprintf('The average number of time steps it takes for the characters to find each other is %.2f steps. \n', avg_oneiterations)
+figure
+hist(oneperson_iterations)
+title('Number of Time Steps - Two People Moving')
+xlabel('Bins')
+ylabel('Frequency')
