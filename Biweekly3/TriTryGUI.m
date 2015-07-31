af334c8e7ade1a0fad34d58d60a61165e0ca4728
r4hermann <bhermann21@comcast.net>
2015-07-31 17:03:57 -0400

    MORE UPDATES

--- Biweekly3/TriTryGUI.m
+++ Biweekly3/TriTryGUI.m
@@ -22,7 +22,7 @@ function varargout = TriTryGUI(varargin)
 
 % Edit the above text to modify the response to help TriTryGUI
 
-% Last Modified by GUIDE v2.5 31-Jul-2015 16:28:53
+% Last Modified by GUIDE v2.5 31-Jul-2015 16:55:16
 
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
@@ -196,3 +196,79 @@ function Thresh_Callback(hObject, eventdata, handles)
 if get(hObject, 'Value') == 1
     handles.countThresh = 1;
 end
+
+
+
+function editable1_Callback(hObject, eventdata, handles)
+% hObject    handle to editable1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of editable1 as text
+%        str2double(get(hObject,'String')) returns contents of editable1 as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function editable1_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to editable1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+
+% --- Executes on button press in RunAlphaGo.
+function RunAlphaGo_Callback(hObject, eventdata, handles)
+% hObject    handle to RunAlphaGo (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)    
+ALPHA= str2double(get(handles.editable1,'String'));
+
+tval=2;
+while tval > 1  % make sure there is an angle inside parameters
+if ALPHA > 1
+set(handles.text1,'String',['Alpha is too small. Enter a larger value.']);
+guidata(hObject, handles);% Update handles variables
+break
+
+elseif ALPHA < 0 
+set(handles.text1,'String',['Alpha is too small. Enter a larger value.']);
+guidata(hObject, handles);% Update handles variables
+break
+
+else
+    tval=0; % if input is inside parameters, this lets program continue to next input step
+end
+end
+
+s=zeros(200,1);
+sfilt=zeros(200,1);
+
+AlphaStyleRaw=plot(s);
+AlphaStyleFilt=plot(sfilt);
+
+axis([0 200 -1.3 1.3]); %Defines axes
+axes(handles.axes1)
+xlabel('Time')
+ylabel('Signal')
+
+while get(hObject, 'value') == 1
+    [gx, gy, gz]=readAcc(handles.accelerometer,handles.calCo); %get data from accel
+
+    xRead=gx; % raw data
+    s=[s(2:end); xRead]; % raw data into set
+    set(AlphaStyleRaw,'ydata',s); % plotting raw dataset
+    
+    % FILTERED AREA 51
+    sfilt_new=(1-ALPHA)*sfilt(end)+ALPHA*gx; % calculating filtered value
+    sfilt=[s(2:end); sfilt_new]; % entering filtered data into 
+    set(AlphaStyleFilt,'ydata',s);% plotting filtered dataset
+    drawnow
+end
+
+guidata(hObject, handles);% Update handles variables
